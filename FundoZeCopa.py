5a367ec6e8c28d1c50dd2bd212725dc5aac01201
Thiago Lechuga <thiagoalz@gmail.com>
2022-08-25 09:31:21 -0300

    Facilitando configurações

--- FundoZeCopa.py
+++ FundoZeCopa.py
@@ -2,20 +2,27 @@ from PIL import Image, ImageFont, ImageDraw
 import datetime
 import os
 
-#Configuracao
-#Voce vera a nova imagem na proxima reunião
+#=====Configuracao=====
+#Voce vera a nova imagem na proxima reunião iniciada
 #Para atualizar todo dia configure seu SO para rodar esse script na inicializacao
 
-#Precisa ver qual a pasta e qual o nome de arquivo que o seu zoom usou para salvar o seu background em uso
+#Precisa descobrir qual a pasta e qual o nome de arquivo que o seu zoom usou para salvar o seu background em uso
+#Vc pode fazer isso adicionando um background novo e verificando a data dos arquivos 
 arquivo_fundo_zoom = "C:\\Users\\99771546\\AppData\\Roaming\\Zoom\\data\\VirtualBkgnd_Custom\\{FFCB4691-A4F1-4031-AD05-C62BA8B7510D}"
 
-#Dias para copa
-date_copa = datetime.date(day=20, month=11, year=2022)
+#Data final
+countdown_date = datetime.date(day=20, month=11, year=2022)
+
+#Frase na imagem
+title_text = "Faltam {} dias para a copa"
+#======================
+
+#Dias para data final
 date_today = datetime.date.today()
-gap_copa = (date_copa-date_today).days
+gap_countdown = (countdown_date-date_today).days
 
 #Montando Imagem
-title_text = "Faltam "+ str(gap_copa) +" dias para a copa"
+title_text = title_text.format(gap_countdown)
 
 my_image = Image.open("FundoZeCopa.png")
 title_font = ImageFont.truetype('GaroaHackerClubeBold.otf', 100)
@@ -25,4 +32,3 @@ my_image.save("FundoZeCopaResult.jpg")
 
 #Mudando no Zoom
 os.replace("FundoZeCopaResult.jpg", arquivo_fundo_zoom)
-
